3404da240ac345e0dc968ec4a6f5acb499f18ce4
Raghavi <raghavi.adoni@cumminscollege.in>
2019-10-25 19:11:36 +0530

    Tabs modified

--- src/components/tabs1.js
+++ src/components/tabs1.js
@@ -15,12 +15,11 @@ function Tabs1()
     </TabList>
 
     <TabPanel>
-      <div class="fluid-container">
-      <div class="column"><Card className = 'about-border'>
+      <Card className = 'about-border'>
   <Card.Body>
     <Card.Title><h2>Mess Manager</h2></Card.Title>
     <Card.Text>
-      <h6 style={{overflowY : 'scroll'}}>Android App built to help easen the ordering process in the mess and to make it more efficient. Built with proper 
+      <h6>Android App built to help easen the ordering process in the mess and to make it more efficient. Built with proper 
         authetication to ensure only the residents have the access to it.
       </h6>
     </Card.Text>
@@ -30,7 +29,7 @@ function Tabs1()
   <Card.Body >
     <Card.Title><h2>Internship at Goldman Sachs</h2></Card.Title>
     <Card.Text>
-      <h6 style={{overflowY : 'scroll'}}>Did my summer internship (2019) at Goldman Sachs which provided me with a great learning oppurtunity. </h6>
+      <h6>Did my summer internship (2019) at Goldman Sachs which provided me with a great learning oppurtunity. </h6>
     </Card.Text>
   </Card.Body>
 </Card>
@@ -38,12 +37,12 @@ function Tabs1()
   <Card.Body>
     <Card.Title><h2>Car Class Prediction </h2></Card.Title>
     <Card.Text>
-      <h6 style={{overflowY : 'scroll'}}>A supervised learning approach to 
+      <h6>A supervised learning approach to 
         predict the type of car based on evaluation of features using supervised learning algorithms and 
         then choosing the best model.</h6>
     </Card.Text>
   </Card.Body>
-</Card></div></div>
+</Card>
     </TabPanel>
     <TabPanel>
       <h2 className="tabs-content">Projects/Experience</h2>
