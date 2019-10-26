3decafefcf96d35c3dc197afd14c23981e5cd11c
Raghavi-Adoni <37770181+Raghavi-Adoni@users.noreply.github.com>
2019-10-26 16:36:32 +0530

    Logo import modified.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './round-profile.jpeg';
+import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import HomeTabs from './components/HomeTabs';
